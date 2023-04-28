46d2a96406911f17e228713e29985867e9b3724b
Yael Valdez <yaelvaldez@gmail.com>
2023-04-28 20:38:46 -0300

    -

--- Full-Stack/3 - POO/EjerciciosPOO/src/Ejercicio4/Ejercicio4_main.java
+++ Full-Stack/3 - POO/EjerciciosPOO/src/Ejercicio4/Ejercicio4_main.java
@@ -13,18 +13,18 @@ import Ejercicio4.Entity.Rectangulo;
 import Ejercicio4.Service.ServiceRectangulo;
 
 public class Ejercicio4_main {
-	
+
 	public static void main(String[] args) {
-		
+
 		ServiceRectangulo x = new ServiceRectangulo();
-		
+
 		Rectangulo obj = x.crearRectangulo();
-		
+
 		System.out.println("La superficie del rectangulo es: " + x.superficie(obj));
-		
+
 		System.out.println("El perimetro del rectangulo es: " + x.perimetro(obj));
-		
+
 		x.dibujarRectangulo(obj);
 	}
-	
+
 }
