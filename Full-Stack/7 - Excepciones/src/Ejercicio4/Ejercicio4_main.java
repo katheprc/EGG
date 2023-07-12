f7dfaaabbea1327b2694ecdfba4ce2da1c8d241a
katheprc <yaelvaldez@gmail.com>
2023-07-11 21:35:32 -0300

    -

---
+++ Full-Stack/7 - Excepciones/src/Ejercicio4/Ejercicio4_main.java
@@ -0,0 +1,14 @@
+/*Todas estas operaciones puede tirar excepciones a manejar, el ingreso por teclado puede
+causar una excepción de tipo InputMismatchException, el método Integer.parseInt() si la cadena
+no puede convertirse a entero, arroja una NumberFormatException y además, al dividir un
+número por cero surge una ArithmeticException. Manipule todas las posibles excepciones
+utilizando bloques try/catch para las distintas excepciones*/
+
+
+package Ejercicio4;
+
+public class Ejercicio4_main {
+	
+	//HECHO EN EJERCICIO 3 :D
+	
+}
